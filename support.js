028a57d8838b550f90dd88dc97914a3abe510d3b
JoenZhu <zhu.joen@gmail.com>
2014-08-29 13:30:30 +0800

    Update support.js

--- support.js
+++ support.js
@@ -5,3 +5,16 @@ function setLeft(i, j){
 function setTop(i, j){
   return 20 + j * 120 ;
 }
+
+function isNoSpace(){
+  var i, j ; 
+  for(i = 0; i < 4; i++){
+    for(j = 0; j < 4; j++){
+      if(data[i][j] == 0){
+        return false ;
+      }
+    }
+  }
+  
+  return true ;
+}
