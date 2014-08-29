9f2dbbf5f02ba887174897226bbd3ac8fd643b03
JoenZhu <zhu.joen@gmail.com>
2014-08-29 11:11:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-var data = [] ,
-    score = 0 ;
+var data = [] , // game data here
+    score = 0 , // game score here
+    
+    $mainContent ; // game main body here
+    
+$(document).ready(function(){
+    $mainContent = $('#mainContent') ;
+    newGame() ; // create a new game 
+}) ;
+
+// create a new game
+function newGame(){
+    initGrid() ; // this will init the main body grid cell 
+}
+
+function initGrid(){
+    var i, j; 
+    
+    // loop to position each grod cell
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j ++){
+            
+        }
+    }
+}
